9da627299e3dc6162f37625840568902f64e7d6e
Seillluuuu <yokpo4@hanmail.net>
2024-05-30 20:27:28 +0900

    Add.myproject
    프로젝트를 추가했습니다.

---
+++ Assets/Scripts/PlayerCondition.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class PlayerCondition : MonoBehaviour
+{
+    public UICondition uiCondition;
+
+    Condition health { get { return uiCondition.health; } }
+    Condition hunger { get { return uiCondition.hunger; } }
+    Condition stamina { get { return uiCondition.stamina; } }
+
+    public float noHungerHealthDecay;
+
+    private void Update()
+    {
+        hunger.Subtract(hunger.passiveValue * Time.deltaTime);
+        stamina.Add(stamina.passiveValue * Time.deltaTime);
+
+        if (hunger.curValue == 0f)
+        {
+            health.Subtract(noHungerHealthDecay * Time.deltaTime);
+        }
+
+        if (health.curValue == 0f)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(float amout)
+    {
+        health.Add(amout);
+    }
+
+    public void Eat(float amout)
+    {
+        hunger.Add(amout);
+    }
+
+    public void Die()
+    {
+        Debug.Log("플레이어가 죽었다.");
+    }
+}
